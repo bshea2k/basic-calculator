89f397ec73093317e9355392bb417a9897d91f9b
Bryant Shea <76180024+bshea2k@users.noreply.github.com>
2020-12-18 13:37:14 -0800

    Add files via upload

---
+++ basicCalculator/basicCalculator.java
@@ -0,0 +1,12 @@
+//basicCalculator.java
+//Bryant Shea
+//Computes basic functions
+
+import java.util.Scanner;
+
+public class basicCalculator {
+
+   public static void main(String[] args) {
+      
+   }
+}
