4248650c339a3e35d736b9873ec7732dfb6d821b
Michael <74842236+MichaelK14@users.noreply.github.com>
2021-01-27 11:46:27 -0800

    Update basicCalculator.java

--- basicCalculator/basicCalculator.java
+++ basicCalculator/basicCalculator.java
@@ -7,6 +7,25 @@ import java.util.Scanner;
 public class basicCalculator {
 
    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      double numberOne, numberTwo, numberThree;
+      String operator;
+      System.out.print("Please enter your first number: ");
+      numberOne = input.nextDouble();
+      System.out.print("Please enter your second number: ");
+      numberTwo = input.nextDouble();
+      System.out.print("Please enter your operator in the forms of \"+\", \"-\", \"/\", and \"*\": ");
+      operator = input.nextLine();
+      
+      if (operator == "*"){
+      System.out.println(numberOne * numberTwo);
+      }if (operator == "+"){
+      System.out.println(numberOne + numberTwo);
+      }if (operator == "-"){
+      System.out.println(numberOne - numberTwo);
+      }if (operator == "/"){
+      System.out.println(numberOne / numberTwo);
+      }
       
    }
 }
